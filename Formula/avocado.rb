74f9997b98ffefeefa244048addcec3644cf5a3b
Rinto <rinto@Rintos-MacBook-Pro.local>
2026-08-03 15:06:01 +0530

    fix(formula): Update Homebrew Formula sha256 to 1e1e4806fedec9d2d607338f8523ced13a34554ca45a4b9da702c5ffab1573d4

--- Formula/avocado.rb
+++ Formula/avocado.rb
@@ -2,7 +2,7 @@ class Avocado < Formula
   desc "Native macOS Terminal Dashboard & CLI App"
   homepage "https://github.com/RnR-io/Avacado"
   url "https://github.com/RnR-io/Avacado/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "ef17a63390ed1421c1ef6db6ed46c47a9a4a36856ac74b5650bd3062705bea3d"
+  sha256 "1e1e4806fedec9d2d607338f8523ced13a34554ca45a4b9da702c5ffab1573d4"
   license "MIT"
 
   depends_on "python@3"
